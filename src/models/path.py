be4fcbf708b10a366ca8893fe8eaf10714c5071b
atroriusss <artorius.ua@gmail.com>
2024-02-11 21:14:09 +0200

    +predict.py

--- src/models/path.py
+++ src/models/path.py
@@ -3,3 +3,6 @@ import os
 
 cur_dir       =   Path(__file__).resolve()
 CONFIG_PATH   =   cur_dir.parents[2] / 'data' / 'processed' / 'config.yaml'
+WEIGHTS_PATH  =   cur_dir.parents[3] / 'runs' / 'detect' / 'train' / 'weights' / 'best.pt'
+TEST_IMGS_DIR =   cur_dir.parents[2] / 'data' / 'test'
+REPORTS_DIR   =   cur_dir.parents[2] / 'reports' / 'submission.csv'
